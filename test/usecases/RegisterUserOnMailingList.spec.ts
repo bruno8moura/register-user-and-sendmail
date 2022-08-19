a69285648bcacbb9715cfe8e37aab35fd3bb80a7
Bruno Moura <bruno8moura@gmail.com>
2022-08-19 05:49:50 -0300

    refactor: add user with complete data to mailing list

--- test/usecases/RegisterUserOnMailingList.spec.ts
+++ test/usecases/RegisterUserOnMailingList.spec.ts
@@ -2,25 +2,38 @@ import { UserRepository } from '../../src/usecases/ports/UserRepository'
 import { RegisterUserOnMailingList } from '../../src/usecases/RegisterUserOnMailingList'
 import { UserData } from '../../src/usecases/UserData'
 
-describe('Register use on mailing list use case', () => {
-  test('should add user with complete data to mailing list', async () => {
-    class InMemoryUserRepository implements UserRepository {
-      private repository: UserData[] = []
-      async add (userData: UserData): Promise<void> {
-        this.repository.push(userData)
-      }
-
-      async findUserByEmail (email: string): Promise<UserData> {
-        return Promise.resolve(this.repository.find(aUser => aUser.email === email))
-      }
+const makeInMemoryRepository = () => {
+  class InMemoryUserRepository implements UserRepository {
+    private repository: UserData[] = []
+    async add (userData: UserData): Promise<void> {
+      this.repository.push(userData)
     }
 
-    const repository: UserRepository = new InMemoryUserRepository()
-    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repository)
+    async findUserByEmail (email: string): Promise<UserData> {
+      return Promise.resolve(this.repository.find(aUser => aUser.email === email))
+    }
+  }
+
+  return new InMemoryUserRepository()
+}
+
+const makeSut = () => {
+  const repository = makeInMemoryRepository()
+  const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repository)
+  return {
+    useCase,
+    repository
+  }
+}
+
+describe('Register use on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const { useCase, repository } = makeSut()
     const name = 'any_name'
     const email = 'any@email.com'
-    await usecase.registerUserOnMailingList({ name, email })
+    const response = await useCase.registerUserOnMailingList({ name, email })
     const user = await repository.findUserByEmail('any@email.com')
     expect(user.name).toBe('any_name')
+    expect(response).toBeUndefined()
   })
 })
