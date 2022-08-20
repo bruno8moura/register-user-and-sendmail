079abb8e2f04b1eb29d004f8baf66171306ce26a
Bruno Moura <bruno8moura@gmail.com>
2022-08-20 05:45:52 -0300

    refactor: not accept empty local part

--- src/domain/Email.ts
+++ src/domain/Email.ts
@@ -9,9 +9,7 @@ export class Email {
 
     const [local, domain] = input.split('@')
 
-    if (local.length === 0) return false
-
-    if (local.length > 64) return false
+    if (local.length > 64 || local.length === 0) return false
 
     if (domain.length > 255 || domain.length === 0) return false
 
