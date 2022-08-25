be5b08dbdd53dc99ad6cb9a003b74ce552a7cd98
Bruno Moura <bruno8moura@gmail.com>
2022-08-25 05:27:13 -0300

    chore: run test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,13 @@
-/*
-* For a detailed explanation regarding each configuration property, visit:
-* https://jestjs.io/docs/en/configuration.html
-*/
-
 module.exports = {
   roots: [
     '<rootDir>/test'
   ],
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!**/test/**',
+    '!**/config/**'
+  ],
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
