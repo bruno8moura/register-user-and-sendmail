61d551d8cbd1dd18b2ddadcea2883748fb4044c2
Bruno Moura <bruno8moura@gmail.com>
2022-09-06 05:37:33 -0300

    test: user exists into database

---
+++ test/external/repositories/mongodb/MongoUserRepository.test.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from '@/external/repositories/mongodb/helpers/MongoHelper'
+import { MongodbUserRepository } from '@/external/repositories/mongodb/MongodbUserRepository'
+import { UserRepository } from '@/usecases'
+
+describe('External :: Repositories :: Mongodb :: MongoUserRepository', () => {
+  beforeEach(() => {
+    MongoHelper.clearCollection('users')
+  })
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('should user exists into database', async () => {
+    const userRepository: UserRepository = new MongodbUserRepository()
+    await userRepository.add({
+      name: 'Any Name',
+      email: 'any@mail.com'
+    })
+
+    expect(await userRepository.exists('any@mail.com')).toBeTruthy()
+  })
+})
