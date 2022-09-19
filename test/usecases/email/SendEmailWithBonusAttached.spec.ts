65b82361726b0e1addfd413c3aa02d250e6b60f6
Bruno Moura <bruno8moura@gmail.com>
2022-09-19 04:31:41 -0300

    test: not send an email with invalid email

--- test/usecases/email/SendEmailWithBonusAttached.spec.ts
+++ test/usecases/email/SendEmailWithBonusAttached.spec.ts
@@ -1,11 +1,13 @@
 import { UserModel } from '@/domain'
-import { Either, right } from '@/shared/util/Either'
+import { Either, left, right } from '@/shared/util/Either'
+import { Left } from '@/shared/util/Either/Left'
 import { IResponse, SendEmailWithBonusAttached } from '@/usecases/email/SendEmailWithBonusAttached'
+import { EmailNotSentError } from '@/usecases/errors/EmailNotSentError'
 import { IEmailSenderService, IEmailSenderServiceResponse, IMessage } from '@/usecases/ports/IEmailSenderService'
 
 const makeEmailSenderServiceStub = (): IEmailSenderService => {
   class EmailSenderServiceStub implements IEmailSenderService {
-    send (message: IMessage): Promise<Either<Error, IEmailSenderServiceResponse>> {
+    send (message: IMessage): Promise<Either<EmailNotSentError, IEmailSenderServiceResponse>> {
       return Promise.resolve(right({
         destination: message.destination,
         sended: true,
@@ -17,16 +19,20 @@ const makeEmailSenderServiceStub = (): IEmailSenderService => {
   return new EmailSenderServiceStub()
 }
 
+let emailSender: IEmailSenderService
+
 describe('Usercase :: SendEmailWithBonusAttached', () => {
+  beforeEach(() => {
+    emailSender = makeEmailSenderServiceStub()
+  })
+
   test('should send an email with bonus attached', async () => {
     const expected: Either<Error, IResponse> = right({
       sended: true,
-      detail: 'Email has been sended to any@email.com',
+      detail: 'Email has been sent to any@email.com',
       destination: 'any@email.com',
       attached: true
     })
-
-    const emailSender = makeEmailSenderServiceStub()
 
     const user: UserModel = {
       id: '123abc',
@@ -41,5 +47,26 @@ describe('Usercase :: SendEmailWithBonusAttached', () => {
     expect(result.isRight()).toBeTruthy()
     expect(result.isLeft()).toBeFalsy()
     expect(result.value).toStrictEqual(expected.value)
+  })
+
+  test('should not send an email with invalid email', async () => {
+    const expected = new EmailNotSentError({ input: 'any@email.com' })
+
+    jest.spyOn(emailSender, 'send').mockResolvedValueOnce(left(expected))
+
+    const user: UserModel = {
+      id: '123abc',
+      name: 'Any User',
+      email: 'any@email.com'
+    }
+
+    const result = await new SendEmailWithBonusAttached(emailSender).execute({
+      user
+    })
+
+    expect(result).toBeInstanceOf(Left)
+    expect(result.isRight()).toBeFalsy()
+    expect(result.isLeft()).toBeTruthy()
+    expect(result.value).toStrictEqual(expected)
   })
 })
