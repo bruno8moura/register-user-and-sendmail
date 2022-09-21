c56b4ee8684c89d9ff572b979fe5fc7af979aeab
Bruno Moura <bruno8moura@gmail.com>
2022-09-21 06:20:52 -0300

    test: return erro when email is not sent

--- test/external/mail-services/nodemailer/helpers/NodeMailerHelper.test.ts
+++ test/external/mail-services/nodemailer/helpers/NodeMailerHelper.test.ts
@@ -1,3 +1,4 @@
+import { SendEmailError } from '@/external/mail-services/errors/SendEmailError'
 import { NodeMailerHelper } from '@/external/mail-services/nodemailer/helpers/NodeMailerHelper'
 
 jest.mock('nodemailer')
@@ -10,6 +11,10 @@ nodemailer.createTransport.mockReturnValue({
 })
 
 describe('External :: Mail-Services :: NodeMailer :: Helpers :: NodeMailerHelper', () => {
+  beforeEach(() => {
+    nodemailer.createTransport.mockClear()
+  })
+
   test('should send an email', async () => {
     const expected = { sended: true }
 
@@ -34,6 +39,43 @@ describe('External :: Mail-Services :: NodeMailer :: Helpers :: NodeMailerHelper
       ]
     })
 
+    expect(result.value).toStrictEqual(expected)
+  })
+
+  test('should return erro when email is not sent', async () => {
+    const expected = new SendEmailError({ input: 'An error happened' })
+
+    const nodeMailerHelper = new NodeMailerHelper({
+      host: 'any',
+      port: 0,
+      auth: {
+        user: 'any',
+        pass: 'any'
+      }
+    })
+
+    nodemailer.createTransport.mockReturnValue({
+      sendMail: jest.fn().mockImplementation(
+        obj => {
+          throw new Error('An error happened')
+        }
+      )
+    })
+
+    const result = await nodeMailerHelper.send({
+      to: 'any_to@email.com',
+      from: 'any_from@email.com',
+      html: '<html>Hello World!</html>',
+      subject: 'A subject',
+      text: 'A text',
+      attachments: [
+        { a: 1 },
+        { b: 2 }
+      ]
+    })
+
+    expect(result.isLeft()).toBeTruthy()
+    expect(result.isRight()).toBeFalsy()
     expect(result.value).toStrictEqual(expected)
   })
 })
