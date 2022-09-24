ee4cafec4cdb51c85b4e6ec87b5adddcb3b38c3e
Bruno Moura <bruno8moura@gmail.com>
2022-09-24 06:34:47 -0300

    fix: get variables MONGO_URL, PORT from the right path

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,7 +4,7 @@ import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
 import { hostname } from 'os'
 
-const { MONGO_URL, PORT } = env
+const { database: { MONGO_URL, PORT } } = env
 
 MongoHelper.connect(MONGO_URL).then(() => {
   app.listen(PORT, () => console.log(`Server running at http://${hostname}:${PORT}`))
