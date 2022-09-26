eaf06bf432309ad7782a2bcf858adfddb8d0210a
Bruno Moura <bruno8moura@gmail.com>
2022-09-26 06:12:11 -0300

    fix: set env variables before start application

--- jest-integration-config.js
+++ jest-integration-config.js
@@ -1,4 +1,11 @@
 const config = require('./jest.config')
 config.testMatch = ['**/*.test.ts']
 
+process.env.SMTP_SERVER_URL = '0.0.0.0'
+process.env.SMTP_SERVER_PORT = '0'
+process.env.SMTP_CREDENTIAL_USER = 'any'
+process.env.SMTP_CREDENTIAL_PASS = 'any'
+process.env.EMAIL_SENDER = 'any'
+process.env.EMAIL_ATTACHED_FILE = 'any'
+
 module.exports = config
