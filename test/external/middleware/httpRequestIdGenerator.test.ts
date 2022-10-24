84b1534f071615c21be9270e2f7ac20901b91c4a
Bruno Moura <bruno8moura@gmail.com>
2022-10-24 04:59:19 -0300

    test: use request id already generated from "x-request-id" header

--- test/external/middleware/httpRequestIdGenerator.test.ts
+++ test/external/middleware/httpRequestIdGenerator.test.ts
@@ -1,7 +1,7 @@
 import { genReqId } from '@/external/middleware/httpRequestIdGenerator'
 import crypto from 'crypto'
 
-describe.only('External :: Middleware :: HttpRequesIdGenerator', () => {
+describe('External :: Middleware :: HttpRequesIdGenerator', () => {
   afterEach(() => {
     jest.clearAllMocks()
   })
@@ -50,5 +50,29 @@ describe.only('External :: Middleware :: HttpRequesIdGenerator', () => {
     expect(res.setHeader).toBeCalledTimes(1)
     expect(result).toBe(uuidV4)
     expect(req.id).toBe(result)
+  })
+
+  test('should use request id already generated from "x-request-id" header', () => {
+    const uuidV4 = 'b359111c-2474-49d8-93d8-7b7f0d31b84e'
+    const req = {
+      id: undefined,
+      headers: {
+        'x-request-id': uuidV4
+      }
+    }
+
+    const res = {
+      setHeader: () => {}
+    }
+
+    jest.spyOn(res, 'setHeader')
+
+    // @ts-ignore
+    const result = genReqId(req, res)
+
+    expect(crypto.randomUUID).toBeCalledTimes(0)
+    expect(res.setHeader).toBeCalledTimes(1)
+    expect(result).toBe(uuidV4)
+    expect(req.id).toBe(undefined)
   })
 })
